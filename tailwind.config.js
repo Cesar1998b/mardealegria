f63d28eb6bfd41a075473d539cf0fe782a051f7a
Cesar Botina <cesarbotina98@gmail.com>
2022-06-24 19:17:43 -0500

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      transitionDuration: {
+        0: "0ms",
+        2000: "2000ms",
+        4000: "4000ms",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+    display: ["responsive", "group-hover", "group-focus"],
+  },
+
+  plugins: [],
+};
